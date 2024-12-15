5ea14f5c47e535c71f9ebfe57aba53f7bbe71006
developer-austine <152804218+developer-austine@users.noreply.github.com>
2024-12-15 19:38:31 +0300

    forms and CSRF

--- routes/web.php
+++ routes/web.php
@@ -8,16 +8,31 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function (){
-    $jobs = Job::with('employer')->cursorPaginate(3); //eager loading || this solves the n+1 problem in terms of sql statement execution
+    $jobs = Job::with('employer')->latest()->simplePaginate(3); //eager loading || this solves the n+1 problem in terms of sql statement execution
 
     return view('jobs', [
         'jobs' => $jobs
     ]);
 });
 
+Route::get('/jobs/create', function() {
+    return view('jobs.create'); // a view to create a job
+});
+
 Route::get('/jobs/{id}', function($id){
     $job = Job::find($id);
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]); // a view for a single job
+});
+
+Route::post('/jobs', function() {
+    // validate....
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+
+    return redirect('/jobs');
 });
 
 //assignment one of loading the contact page
